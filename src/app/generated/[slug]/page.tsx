67664a5578cb025cea99b7c226e47a0f5e4af9e6
Dan Overton <dan@galatasoftware.co.uk>
2024-04-30 21:18:55 +0100

    Add metadata

--- src/app/generated/[slug]/page.tsx
+++ src/app/generated/[slug]/page.tsx
@@ -2,6 +2,29 @@ export async function generateStaticParams() {
   return [];
 }
 
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const res = await fetch(
+    process.env.VERCEL_ENV
+      ? `https://${process.env.VERCEL_URL}/api/get-time`
+      : "http://localhost:3000/api/get-time",
+    {
+      method: "POST",
+      body: JSON.stringify({ slug: params.slug }),
+      next: { tags: [`time-${params.slug}`] },
+    }
+  );
+
+  const data = await res.json();
+
+  return {
+    title: `${params.slug} - ${data.time}`,
+  };
+}
+
 export default async function Page({ params }: { params: { slug: string } }) {
   const res = await fetch(
     process.env.VERCEL_ENV
